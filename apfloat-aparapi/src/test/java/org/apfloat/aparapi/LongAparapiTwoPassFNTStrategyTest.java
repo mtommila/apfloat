8e3815029b090be12bf60324a157e90f01cb5675
Mikko Tommila <mikko.tommila@apfloat.org>
2017-02-05 11:50:40 +0200

    Add unit tests for aparapi two-pass transforms

---
+++ apfloat-aparapi/src/test/java/org/apfloat/aparapi/LongAparapiTwoPassFNTStrategyTest.java
@@ -0,0 +1,79 @@
+/*
+ * Apfloat arbitrary precision arithmetic library
+ * Copyright (C) 2002-2017  Mikko Tommila
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package org.apfloat.aparapi;
+
+import org.apfloat.*;
+import org.apfloat.spi.*;
+import org.apfloat.internal.*;
+
+import junit.framework.TestSuite;
+
+/**
+ * @version 1.8.3
+ * @author Mikko Tommila
+ */
+
+public class LongAparapiTwoPassFNTStrategyTest
+    extends LongNTTStrategyTestCase
+{
+    public LongAparapiTwoPassFNTStrategyTest(String methodName)
+    {
+        super(methodName);
+    }
+
+    public static void main(String[] args)
+    {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite()
+    {
+        TestSuite suite = new TestSuite();
+
+        suite.addTest(new LongAparapiTwoPassFNTStrategyTest("testRoundTrip"));
+        suite.addTest(new LongAparapiTwoPassFNTStrategyTest("testRoundTripBig"));
+
+        return suite;
+    }
+
+    public static void testRoundTrip()
+    {
+        ApfloatContext ctx = ApfloatContext.getContext();
+
+        ctx.setMaxMemoryBlockSize(65536);
+        ctx.setMemoryThreshold(1024);
+        ctx.setBlockSize(256);
+        runRoundTrip(131072);
+    }
+
+    public static void testRoundTripBig()
+    {
+        ApfloatContext ctx = ApfloatContext.getContext();
+
+        ctx.setMaxMemoryBlockSize(65536);
+        ctx.setMemoryThreshold(1024);
+        ctx.setBlockSize(256);
+        runRoundTrip((int) Math.min(1 << 21, Util.round2down(LongModConstants.MAX_TRANSFORM_LENGTH)));
+    }
+
+    private static void runRoundTrip(int size)
+    {
+        runRoundTrip(new ColumnTwoPassFNTStrategy(new LongAparapiNTTStepStrategy()), size);
+    }
+}
