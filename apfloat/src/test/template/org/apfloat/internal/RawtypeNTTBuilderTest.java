a5a8d5a286fb34ca514796eae44ac11cdbf53357
Mikko Tommila <mikko.tommila@apfloat.org>
2018-08-31 00:29:37 +0300

    Fix unstable test

--- apfloat/src/test/template/org/apfloat/internal/RawtypeNTTBuilderTest.java
+++ apfloat/src/test/template/org/apfloat/internal/RawtypeNTTBuilderTest.java
@@ -24,7 +24,7 @@ import org.apfloat.spi.*;
 import junit.framework.TestSuite;
 
 /**
- * @version 1.7.0
+ * @version 1.9.0
  * @author Mikko Tommila
  */
 
@@ -58,8 +58,8 @@ public class RawtypeNTTBuilderTest
         long maxMemoryBlockSize = ctx.getMaxMemoryBlockSize() / sizeof(rawtype);
 
         assertTrue("Fits in cache", nttBuilder.createNTT(cacheSize / 2) instanceof RawtypeTableFNTStrategy);
-        assertTrue("Fits in memory", nttBuilder.createNTT(maxMemoryBlockSize) instanceof SixStepFNTStrategy);
-        assertTrue("Does not fit in memory", nttBuilder.createNTT(maxMemoryBlockSize * 2) instanceof TwoPassFNTStrategy);
+        assertTrue("Fits in memory", nttBuilder.createNTT(Util.round2down(maxMemoryBlockSize)) instanceof SixStepFNTStrategy);
+        assertTrue("Does not fit in memory", nttBuilder.createNTT(Util.round2down(maxMemoryBlockSize * 2)) instanceof TwoPassFNTStrategy);
         assertTrue("Factor 3", nttBuilder.createNTT(3) instanceof Factor3NTTStrategy);
 
         assertTrue("NTTStepStrategy", nttBuilder.createNTTSteps() instanceof NTTStepStrategy);
