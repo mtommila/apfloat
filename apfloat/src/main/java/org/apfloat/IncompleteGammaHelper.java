3a6a6d00177545ee429c8401689fe91e2804d54a
Mikko Tommila <mikko.tommila@apfloat.org>
2020-11-18 21:49:14 +0200

    Add comments

--- apfloat/src/main/java/org/apfloat/IncompleteGammaHelper.java
+++ apfloat/src/main/java/org/apfloat/IncompleteGammaHelper.java
@@ -1,9 +1,29 @@
+/*
+ * Apfloat arbitrary precision arithmetic library
+ * Copyright (C) 2002-2020  Mikko Tommila
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 package org.apfloat;
 
 import java.util.function.BiFunction;
 import java.util.function.LongFunction;
 
 /**
+ * Helper class for the incomplete gamma function.
+ *
  * @since 1.10.0
  * @version 1.10.0
  * @author Mikko Tommila
